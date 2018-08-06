5a633bb8b4d7a057bb4d18a1fe7cec04d4d5e036
Vishnu <babycasper123@gmail.com>
2018-08-06 07:34:38 +0530

    06-08-18|07:34AM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./shared/layout/header/header.component";
+import { LoginComponent } from "./components/login/login.component";
+
+const appRoutes: Routes = [
+  {
+    path: "",
+    component: AppComponent
+  },
+  { path: "login", component: LoginComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, LoginComponent],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
